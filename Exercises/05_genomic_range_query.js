aef7864406a6cb7809e8c9e98380fcdefecdb327
Oleksandr Avoiants <shhavel@gmail.com>
2019-11-01 23:35:38 +0200

    Lesson #5 Task #2 Optimize

--- Exercises/05_genomic_range_query.js
+++ Exercises/05_genomic_range_query.js
@@ -4,16 +4,37 @@ const IMPACT_FACTOR = { A: 1, C: 2, G: 3, T: 4 };
 
 function solution(S, P, Q) {
   const impactFactors = S.split('').map(n => IMPACT_FACTOR[n]);
-  const N = P.length;
-  const minimalImpactFactors = new Array(N);
-  let p, q, m;
-  for (let i = 0; i < N; i++) {
-    p = P[i]; q = Q[i];
-    m = impactFactors[q];
-    for (let j = p; j < q; j++) {
-      m = Math.min(m, impactFactors[j]);
+  const N = impactFactors.length;
+  const prefMaxAInxs = new Array(N);
+  const prefMaxCInxs = new Array(N);
+  const prefMaxGInxs = new Array(N);
+  let maxAIdx = -1, maxCIdx = -1, maxGIdx = -1, i, p, q;
+  for (i = 0; i < N; i++) {
+    if (impactFactors[i] === 1) {
+      maxAIdx = i;
+    } else if (impactFactors[i] === 2) {
+      maxCIdx = i;
+    } else if (impactFactors[i] === 3) {
+      maxGIdx = i;
     }
-    minimalImpactFactors[i] = m;
+    prefMaxAInxs[i] = maxAIdx;
+    prefMaxCInxs[i] = maxCIdx;
+    prefMaxGInxs[i] = maxGIdx;
+  }
+  const I = P.length;
+  const minimalImpactFactors = new Array(I);
+  for (i = 0; i < I; i++) {
+    p = P[i]; q = Q[i];
+    // Find minimum value in a range.
+    if (prefMaxAInxs[q] >= p) {
+      minimalImpactFactors[i] = 1;
+    } else if (prefMaxCInxs[q] >= p) {
+      minimalImpactFactors[i] = 2;
+    } else if (prefMaxGInxs[q] >= p) {
+      minimalImpactFactors[i] = 3;
+    } else {
+      minimalImpactFactors[i] = 4;
+    }
   }
   return minimalImpactFactors;
 }
